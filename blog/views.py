2cc780dc56757988f6fbb4393b1d9fecdfc0824c
eqbal <comtech9064@gmail.com>
2020-05-31 03:35:45 +0430

    add register page

--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,6 @@ from django.http import HttpResponse
 from .models import Post
 
 # Create your views here. ORM - object orented Matadate / sqllite3
-posts = [
-    {
-        'author': 'Eqbal amini',
-        'title': 'blog post 1',
-        'content': 'First post content',
-        'date_posted': 'may 28, 2020'
-    },
-    {
-        'author': 'Eqbal amini',
-        'title': 'blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'may 28, 2020'
-    },
-]
 
 def home(request):
     context = {
